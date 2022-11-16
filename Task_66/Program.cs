b831b6f057177be0a39c3aef0496781c5c99b67f
Dmitriy <potapidze.mos@mail.ru>
2022-11-16 20:59:42 +0300

    Добавил решение задачи №66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -6,3 +6,21 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void NaturalElem(int num1, int num2, int sum)
+{
+    if (num1 > num2)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + (num1++);
+    NaturalElem(num1, num2, sum);
+}
+NaturalElem(num1, num2, 0);
